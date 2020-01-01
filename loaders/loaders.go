a4995ce86ef617e26020754e5ab21b30d312c84d
Fredrik Åhs <freahs@kth.se>
2020-01-01 13:32:19 +0100

    Add loders
    A data loader for prime burger

---
+++ loaders/loaders.go
@@ -0,0 +1,41 @@
+package loaders
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/snabb/isoweek"
+)
+
+// dayFromString convers Swedish days to their corresponding time.Weekday
+func dayFromString(s string) (time.Weekday, error) {
+	switch strings.ToLower(s) {
+	case "måndag":
+		return time.Monday, nil
+	case "tisdag":
+		return time.Tuesday, nil
+	case "onsdag":
+		return time.Wednesday, nil
+	case "torsdag":
+		return time.Thursday, nil
+	case "fredag":
+		return time.Friday, nil
+	case "lördag":
+		return time.Saturday, nil
+	case "söndag":
+		return time.Sunday, nil
+	default:
+		return -1, fmt.Errorf("%s is not a valid day", s)
+	}
+}
+
+func parseISOWeek(year, week int, day time.Weekday) time.Time {
+	t := isoweek.StartTime(year, week, time.UTC)
+	t = t.AddDate(0, 0, (int(day)+6)%7)
+	return t
+}
+
+func daysUntil(now, then time.Time) int {
+	return int(then.Sub(now.Truncate(time.Hour*24)).Hours() / 24)
+}
---
+++ loaders/prime.go
@@ -0,0 +1,91 @@
+package loaders
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/freahs/lunch-server/data"
+)
+
+// Prime implements the Loader interface and loads menues from an URL
+type Prime struct {
+	URL string
+}
+
+// NewPrime returns a new Prime
+func NewPrime() Prime {
+	return Prime{"https://www.primeburger.se/lunchmeny/"}
+}
+
+// Name implements Loader
+func (p Prime) Name() string {
+	return "prime"
+}
+
+func (p Prime) parseWeekString(s string) (int, error) {
+	parts := strings.Split(s, " ")
+	if len(parts) < 2 {
+		return -1, fmt.Errorf("can't get week number from %s", s)
+	}
+	week, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return -1, fmt.Errorf("illegal week number: '%s'", s)
+	}
+	return week, nil
+}
+
+// Load implements Loader
+func (p Prime) Load(store *data.Store) error {
+	res, err := http.Get(p.URL)
+	if err != nil {
+		return err
+	} else if res.StatusCode != 200 {
+		return fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+
+	defer func() {
+		switch r := recover().(type) {
+		case nil: // do nothing
+		case error:
+			err = r
+		default:
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+
+	doc.Find("h2").Each(func(i int, weekSelector *goquery.Selection) {
+		week, err := p.parseWeekString(weekSelector.Text())
+		if err != nil {
+			panic(err)
+		}
+		year, currentWeek := now.ISOWeek()
+		if week < currentWeek-20 {
+			year++
+		}
+		menuSelector := weekSelector.Parent().Find("tr")
+		if menuSelector.Length() == 5 {
+			menuSelector.Each(func(i int, itemSelector *goquery.Selection) {
+				day, err := dayFromString(itemSelector.Find(".column-1").Text())
+				if err != nil {
+					panic(err)
+				}
+				item := itemSelector.Find(".column-2").Text()
+				y, m, d := parseISOWeek(year, week, day).Date()
+				store.AddMenu(data.NewMenu("prime", y, int(m), d, item))
+			})
+		}
+	})
+	return err
+}
